5ce2b40b0608fb14a110b6f996fe812edf8eaec5
MulubahzumuKSipor <sip23001@byui.edu>
2025-09-30 22:35:51 +0000

    Last Commit

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,6 @@
 import type { NextAuthConfig } from 'next-auth';
  
-export const authConfig = {
-  pages: {
-    signIn: '/login',
-  },
-import type { NextAuthConfig } from 'next-auth';
+
  
 export const authConfig = {
   pages: {
